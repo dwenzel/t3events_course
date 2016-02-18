faef1831b46b8783bcf5ddbd3913c4dd698fc9d8
Dirk Wenzel <wenzel@cps-it.de>
2016-02-18 23:06:22 +0100

    [TASK] re-added CourseController->listAction

--- Classes/Controller/Backend/CourseBackendController.php
+++ Classes/Controller/Backend/CourseBackendController.php
@@ -2,6 +2,9 @@
 namespace CPSIT\T3eventsCourse\Controller\Backend;
 
 use CPSIT\T3eventsCourse\Domain\Model\Dto\CourseDemand;
+use TYPO3\CMS\Core\Messaging\FlashMessage;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
+use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
 use Webfox\T3events\Controller\AbstractBackendController;
 
 /**
@@ -12,9 +15,50 @@ use Webfox\T3events\Controller\AbstractBackendController;
 class CourseBackendController extends AbstractBackendController {
 
 	/**
+	 * courseRepository
+	 *
+	 * @var \CPSIT\T3eventsCourse\Domain\Repository\CourseRepository
+	 * @inject
+	 */
+	protected $courseRepository;
+
+	/**
+	 * action list
+	 *
+	 * @param array $overwriteDemand
+	 * @return void
+	 */
+	public function listAction($overwriteDemand = NULL) {
+		$demand = $this->createDemandFromSettings($overwriteDemand);
+		$courses = $this->courseRepository->findDemanded($demand);
+
+		if (($courses instanceof QueryResultInterface AND !$courses->count())
+			OR !count($courses)
+		) {
+			$this->addFlashmessage(
+				$this->translate('message.noCoursesFound.text'),
+				$this->translate('message.noCoursesFound.title'),
+				FlashMessage::WARNING
+			);
+		}
+		$configuration = $this->configurationManager->getConfiguration(
+			ConfigurationManagerInterface::CONFIGURATION_TYPE_FRAMEWORK
+		);
+
+		$this->view->assignMultiple(
+			[
+				'courses' => $courses,
+				'demand' => $demand,
+				'settings' => $this->settings,
+				'storagePid' => $configuration['persistence']['storagePid'],
+			]
+		);
+	}
+
+	/**
 	 * Create Demand from Settings
 	 *
-	 * @param \array $settings
+	 * @param array $settings
 	 * @return \Webfox\T3events\Domain\Model\Dto\EventDemand
 	 */
 	protected function createDemandFromSettings($settings) {
