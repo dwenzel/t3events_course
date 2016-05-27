de7ef304223b8676379a53206d2442c958534ca3
Dirk Wenzel <wenzel@cps-it.de>
2016-05-27 16:04:07 +0200

    [TASK] ScheduleBackendController now extends PerformanceController and uses ModuleDataTrait. Default TypoScript settings and list template adapted.

--- Classes/Controller/Backend/ScheduleBackendController.php
+++ Classes/Controller/Backend/ScheduleBackendController.php
@@ -1,57 +1,32 @@
 <?php
 namespace CPSIT\T3eventsCourse\Controller\Backend;
 
-use CPSIT\T3eventsCourse\Controller\ScheduleController;
+use Webfox\T3events\Controller\ModuleDataTrait;
+use Webfox\T3events\Controller\PerformanceController;
 
 /**
  * Class ScheduleBackendController
  *
  * @package CPSIT\T3eventsCourse\Controller
  */
-class ScheduleBackendController extends ScheduleController {
-
-	/**
-	 * Page uid
-	 *
-	 * @var integer
-	 */
-	protected $pageUid = 0;
-
-	/**
-	 * Initialize Action
-	 * Function will be called before every other action
-	 *
-	 * @return void
-	 */
-	public function initializeAction() {
-		$this->lessonRepository->setDefaultOrderings(array('date' => \TYPO3\CMS\Extbase\Persistence\QueryInterface::ORDER_ASCENDING));
-		$this->pageUid = (int) \TYPO3\CMS\Core\Utility\GeneralUtility::_GET('id');
-		$this->setTsConfig();
-		parent::initializeAction();
-	}
-
-	/**
-	 * Set the TsConfig configuration for the extension
-	 *
-	 * @return void
-	 */
-	protected function setTsConfig() {
-		$tsConfig = \TYPO3\CMS\Backend\Utility\BackendUtility::getPagesTSconfig($this->pageUid);
-		if (isset($tsConfig['module.']['tx_t3eventscourse.']['lesson.']) && is_array($tsConfig['module.']['tx_t3eventscourse.']['lesson.'])) {
-			$this->tsConfiguration = $tsConfig['module.']['tx_t3eventscourse.']['lesson.'];
-		}
-	}
-
+class ScheduleBackendController extends PerformanceController {
+	use ModuleDataTrait;
 
 	/**
 	 * Create Demand from Settings
 	 *
-	 * @param \array $settings
-	 * @return \Webfox\T3events\Domain\Model\Dto\EventDemand
+	 * @param array $settings
+	 * @return \Webfox\T3events\Domain\Model\Dto\PerformanceDemand
 	 */
 	protected function createDemandFromSettings($settings) {
-		$demand = parent::createDemandFromSettings($settings['lesson']['list']);
+        $localSettings = [];
+		$controllerKey = $this->settingsUtility->getControllerKey($this);
+        if (isset($settings[$controllerKey]['list'])) {
+            $localSettings = $settings[$controllerKey]['list'];
+        }
 
-		return $demand;
+        $demand = parent::createDemandFromSettings($localSettings);
+
+        return $demand;
 	}
 }
