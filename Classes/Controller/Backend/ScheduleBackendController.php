c4fca3dfcbb4b21f5944422f5cf194bcd95b2591
Dirk Wenzel <wenzel@cps-it.de>
2016-06-02 16:23:54 +0200

    [TASK] list action of backend module for schedules is now filterable.

--- Classes/Controller/Backend/ScheduleBackendController.php
+++ Classes/Controller/Backend/ScheduleBackendController.php
@@ -4,6 +4,9 @@ namespace CPSIT\T3eventsCourse\Controller\Backend;
 use Webfox\T3events\Controller\ModuleDataTrait;
 use Webfox\T3events\Controller\PerformanceController;
 use Webfox\T3events\Controller\SettingsUtilityTrait;
+use TYPO3\CMS\Extbase\Mvc\Exception\StopActionException;
+use TYPO3\CMS\Extbase\Mvc\RequestInterface;
+use TYPO3\CMS\Extbase\Mvc\ResponseInterface;
 
 /**
  * Class ScheduleBackendController
@@ -13,21 +16,56 @@ use Webfox\T3events\Controller\SettingsUtilityTrait;
 class ScheduleBackendController extends PerformanceController {
 	use ModuleDataTrait, SettingsUtilityTrait;
 
-	/**
-	 * Create Demand from Settings
-	 *
-	 * @param array $settings
-	 * @return \Webfox\T3events\Domain\Model\Dto\PerformanceDemand
-	 */
-	protected function createDemandFromSettings($settings) {
-        $localSettings = [];
-		$controllerKey = $this->settingsUtility->getControllerKey($this);
-        if (isset($settings[$controllerKey]['list'])) {
-            $localSettings = $settings[$controllerKey]['list'];
+    /**
+     * Load and persist module data
+     *
+     * @param \TYPO3\CMS\Extbase\Mvc\RequestInterface $request
+     * @param \TYPO3\CMS\Extbase\Mvc\ResponseInterface $response
+     * @return void
+     * @throws \TYPO3\CMS\Extbase\Mvc\Exception\StopActionException
+     */
+    public function processRequest(RequestInterface $request, ResponseInterface $response)
+    {
+        $this->moduleData = $this->moduleDataStorageService->loadModuleData($this->getModuleKey());
+
+        try {
+            parent::processRequest($request, $response);
+            $this->moduleDataStorageService->persistModuleData($this->moduleData, $this->getModuleKey());
+        } catch (StopActionException $e) {
+            $this->moduleDataStorageService->persistModuleData($this->moduleData, $this->getModuleKey());
+            throw $e;
+        }
+    }
+
+    /**
+     * action list
+     *
+     * @param array $overwriteDemand
+     * @return void
+     */
+    public function listAction(array $overwriteDemand = null)
+    {
+        $demand = $this->createDemandFromSettings($this->settings['scheduleBackend']['list']);
+        $filterOptions = $this->getFilterOptions(
+            $this->settings[$this->settingsUtility->getControllerKey($this)]['list']['filter']
+        );
+
+        if ($overwriteDemand === null) {
+            $overwriteDemand = $this->moduleData->getOverwriteDemand();
+        } else {
+            $this->moduleData->setOverwriteDemand($overwriteDemand);
         }
 
-        $demand = parent::createDemandFromSettings($localSettings);
+        $this->overwriteDemandObject($demand, $overwriteDemand);
 
-        return $demand;
-	}
+        $templateVariables = [
+                'performances' => $this->performanceRepository->findDemanded($demand),
+                'overwriteDemand' => $overwriteDemand,
+                'demand' => $demand,
+                'filterOptions' => $filterOptions
+            ];
+
+        $this->emitSignal(__CLASS__, self::PERFORMANCE_LIST_ACTION, $templateVariables);
+        $this->view->assignMultiple($templateVariables);
+    }
 }
