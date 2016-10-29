740859d364eb3e005a9f60975f2fb648e041671e
Dirk Wenzel <der-wenzel@gmx.de>
2016-10-29 22:00:23 +0200

    [TASK] Hook ItemsProcFunc now extends class from t3events.

--- Classes/Hooks/ItemsProcFunc.php
+++ Classes/Hooks/ItemsProcFunc.php
@@ -1,71 +1,29 @@
 <?php
 namespace CPSIT\T3eventsCourse\Hooks;
 
-/***************************************************************
- *  Copyright notice
- *  (c) 2012 Dirk Wenzel <wenzel@webfox01.de>, Agentur Webfox
- *  Michael Kasten <kasten@webfox01.de>, Agentur Webfox
- *    method user_templateLayout has been taken from extension tx_news by
- *  Georg Ringer
- *  All rights reserved
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
-use TYPO3\CMS\Backend\Utility\BackendUtility;
+    /**
+     * This file is part of the TYPO3 CMS project.
+     *
+     * It is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License, either version 2
+     * of the License, or any later version.
+     *
+     * For the full copyright and license information, please read the
+     * LICENSE.txt file that was distributed with this source code.
+     *
+     * The TYPO3 project - inspiring people to share!
+     */
 
 /**
- * @package t3events_course
- * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ * Class ItemsProcFunc
+ * @package CPSIT\T3eventsCourse\Hooks
  */
-class ItemsProcFunc {
-
-	/**
-	 * Itemsproc function to extend the selection of templateLayouts in the plugin
-	 *
-	 * @param array &$config configuration array
-	 * @return void
-	 */
-	public function user_templateLayout(array &$config) {
-		// Check if the layouts are extended by ext_tables
-		if (isset($GLOBALS['TYPO3_CONF_VARS']['EXT']['tx_t3eventscourse']['templateLayouts'])
-			&& is_array($GLOBALS['TYPO3_CONF_VARS']['EXT']['tx_t3eventscourse']['templateLayouts'])
-		) {
-
-			// Add every item
-			foreach ($GLOBALS['TYPO3_CONF_VARS']['EXT']['t3events_course']['templateLayouts'] as $layouts) {
-				$additionalLayout = [
-					$GLOBALS['LANG']->sL($layouts[0], TRUE),
-					$layouts[1]
-				];
-				array_push($config['items'], $additionalLayout);
-			}
-		}
-
-		// Add tsconfig values
-		if (is_numeric($config['row']['pid'])) {
-			$pagesTsConfig = BackendUtility::getPagesTSconfig($config['row']['pid']);
-			if (isset($pagesTsConfig['tx_t3eventscourse.']['templateLayouts.']) && is_array($pagesTsConfig['tx_t3eventscourse.']['templateLayouts.'])) {
-
-				// Add every item
-				foreach ($pagesTsConfig['tx_t3eventscourse.']['templateLayouts.'] as $key => $label) {
-					$additionalLayout = [
-						$GLOBALS['LANG']->sL($label, TRUE),
-						$key
-					];
-					array_push($config['items'], $additionalLayout);
-				}
-			}
-		}
-	}
+class ItemsProcFunc extends \DWenzel\T3events\Hooks\ItemsProcFunc
+{
+    /**
+     * Key for look up in TYPO3_CONF_VARS and page TS config
+     *
+     * @const EXTENSION_KEY
+     */
+    const EXTENSION_KEY = 't3events_course';
 }
-
