cf38cebd72d9d767971a4a2d657dd1ad201ee205
Dirk Wenzel <der-wenzel@gmx.de>
2019-08-13 18:24:13 +0200

    [TASK] extension configuration refactored - require dwenzel/t3events:~1.1 - require dwenzel/t3extension-tools:~1.2

---
+++ Classes/Configuration/SettingsInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace CPSIT\T3eventsCourse\Configuration;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2019 Dirk Wenzel
+ *  All rights reserved
+ *
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ * A copy is found in the text file GPL.txt and important notices to the license
+ * from the author is found in LICENSE.txt distributed with these scripts.
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use DWenzel\T3events\Utility\SettingsInterface as EventsSettingsInterface;
+
+/**
+ * Interface SettingsInterface
+ */
+interface SettingsInterface extends EventsSettingsInterface
+{
+    public const TABLE_CERTIFICATE = 'tx_t3eventscourse_domain_model_certificate';
+    public const TABLE_CERTIFICATE_TYPE = 'tx_t3eventscourse_domain_model_certificate_TYPE';
+}
